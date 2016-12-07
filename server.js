abe118c2801818e556e5b3bb1ec90ed52827b837
Shirish Kumar <shirish@mysmartprice.com>
2016-12-07 17:23:23 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
-	console.log("received a request");
-	response.write("Hello from the other side");
-	response.end();
+        console.log("received a request");
+        console.log(request);
+        response.write("Hello from the other side");
+        response.end();
 });
 
 server.listen(3000);
